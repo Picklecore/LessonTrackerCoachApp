48c947b0684453f5cc086b63fd06ca47130e1d31
zlin22 <zhi.long.lin@gmail.com>
2026-05-20 00:59:54 -0400

    updated responsive design

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { IOSDevice } from './ios.jsx';
 import './styles.css';
 
-function Root() {
-  return (
-    <IOSDevice width={402} height={874} dark={true}>
-      <App />
-    </IOSDevice>
-  );
-}
-
-createRoot(document.getElementById('root')).render(<Root />);
+createRoot(document.getElementById('root')).render(<App />);
